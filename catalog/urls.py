c57a2bc1ed8ba223cf6b4e287e7f289759addc33
Iv-EN <engenivanov@yandex.ru>
2024-06-25 22:32:42 +0300

    First review

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
+from catalog.views import contacts, homepage
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", include("catalog.urls", namespace="catalog")),
+    path("", homepage, name="homepage"),
+    path("contacts/", contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, "index.html")
+
+
+def contacts(request):
+    if request.method == "POST":
+        user_name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        user_message = request.POST.get("message")
+        filename = f"{user_name}_information.txt"
+        message = f"пользователь {user_name} (почта: {email}, телефон: {phone}) оставил сообщение '{user_message}'"
+        file = open(filename, "w")
+        file.write(message)
+        file.close()
+    return render(request, "contacts.html")
