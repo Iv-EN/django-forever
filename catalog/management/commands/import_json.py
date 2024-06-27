d43c1032fa6c945a323cf0bc3d86ae94e74a9e35
Iv-EN <engenivanov@yandex.ru>
2024-06-27 22:57:42 +0300

    ata has been loaded into the database

---
+++ catalog/management/commands/import_json.py
@@ -0,0 +1,55 @@
+import os.path
+import json
+
+from django.conf import settings
+from django.core.management import BaseCommand
+from django.db import connection
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    help = "Загрузка данных из json файла"
+
+    @staticmethod
+    def clear_model(model):
+        model.objects.all().delete()
+        table_name = model._meta.db_table
+        sequence_sql = f"ALTER SEQUENCE {table_name}_id_seq RESTART WITH 1"
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(sequence_sql)
+        except Exception as e:
+            print(f"При сбросе нумерации `id` произошла ошибка: {e}")
+
+    @staticmethod
+    def get_json_file(filename):
+        return os.path.join(settings.BASE_DIR, "fixtures", filename)
+
+    def handle(self, *args, **options):
+        models = [Product, Category]
+        for model in models:
+            self.clear_model(model)
+
+        fixture_file = self.get_json_file("catalog_data.json")
+        with open(fixture_file, "r", encoding="utf-16") as file:
+            fixtures = json.load(file)
+
+        for fixture in fixtures:
+            if fixture["model"] == "catalog.category":
+                pk = fixture["pk"]
+                fields = fixture["fields"]
+                Category(pk=pk, **fields).save(force_insert=True)
+                print(
+                    f"Категория: <{fields["name"]}> загружена в базу"
+                )
+
+        for fixture in fixtures:
+            if fixture["model"] == "catalog.product":
+                pk = fixture["pk"]
+                fields = fixture["fields"]
+                category_id = fields.pop("category")
+                fields["category"] = Category.objects.get(pk=category_id)
+                Product(pk=pk, **fields).save(force_insert=True)
+                print(f"Продукт <{fields["name"]}> с загружен в базу")
+        print("Все данные успешно занесены в базу")
