f33fe583462ef7f57ca732e90290f67f0d3def5c
Iv-EN <engenivanov@yandex.ru>
2024-06-27 23:49:36 +0300

    Corrected the data loading command

--- catalog/management/commands/import_json.py
+++ catalog/management/commands/import_json.py
@@ -35,21 +35,24 @@ class Command(BaseCommand):
         with open(fixture_file, "r", encoding="utf-16") as file:
             fixtures = json.load(file)
 
-        for fixture in fixtures:
-            if fixture["model"] == "catalog.category":
-                pk = fixture["pk"]
-                fields = fixture["fields"]
-                Category(pk=pk, **fields).save(force_insert=True)
-                print(
-                    f"Категория: <{fields["name"]}> загружена в базу"
-                )
+        categories = []
+        products = []
 
         for fixture in fixtures:
-            if fixture["model"] == "catalog.product":
-                pk = fixture["pk"]
-                fields = fixture["fields"]
-                category_id = fields.pop("category")
-                fields["category"] = Category.objects.get(pk=category_id)
-                Product(pk=pk, **fields).save(force_insert=True)
-                print(f"Продукт <{fields["name"]}> с загружен в базу")
-        print("Все данные успешно занесены в базу")
+            model_name = fixture["model"]
+            pk = fixture["pk"]
+            fields = fixture["fields"]
+            if model_name == "catalog.category":
+                categories.append(Category(pk=pk, **fields))
+            elif model_name == "catalog.product":
+                products.append({"pk": pk, "fields": fields})
+
+        Category.objects.bulk_create(categories)
+        print(f"{len(categories)} категорий занесено в базу")
+        for products_data in products:
+            pk = products_data["pk"]
+            fields = products_data["fields"]
+            category_id = fields.pop("category")
+            fields["category"] = Category.objects.get(pk=category_id)
+            Product(pk=pk, **fields).save(force_insert=True)
+            print(f"продукт <{fields["name"]}> занесён в базу")
